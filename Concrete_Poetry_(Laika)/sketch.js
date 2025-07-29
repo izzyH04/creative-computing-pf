1e82dddd267d5530853873c573f699dc44dcdda6
izzyH04 <izzy.henshaw24@bathspa.ac.uk>
2025-07-29 21:12:16 +0100

    Added Experiment 3 (Concrete Poetry)

---
+++ Concrete_Poetry_(Laika)/sketch.js
@@ -0,0 +1,171 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  translate(200,200);
+  angleMode(DEGREES);
+  background(220);
+  
+  push(); 
+  rotate(15);
+  fill(0,0,255);
+  text('they', -186, -113);
+  text(' s   a   y ', -200, -100);
+  text('that,', -186, -87);
+  pop();
+  
+  push();
+  rotate(5)
+  text('from',-90,-130);
+  text('space,',-85,-120);
+  text('the', -45, -122);
+  text('earth looks', -80, -110);
+  text('a small,', -55, -100);
+  text('b a l l,', -45, -90);
+  text('did it look to', -55, -70);
+  text('you, L a i k a ?', -53,-60);
+  text('from that shuttle like', -70, -50);
+  text('whose string they let', -65, -40);
+  text('which they never trained', -65, -30);
+  text('for recall?', -55, -20);
+  text('they say you were', 0, -10);
+  text('a stray who never', 5, 0);
+  text('and that the clever', 10, 10);
+  text('people    called', 23, 20);
+  text('you pet names', 30, 30);
+  text('through the', 40, 40);
+  text('wires of', 53, 50);
+  text('your', 63, 60);
+  text('shr', 67, 80);
+  text('ink', 69, 90);
+  text('ing', 70, 100);
+  text('cages,', 57, 115);
+  pop();
+  
+  push();
+  rotate(5);
+  fill(255,0,0);
+  text('but how', -50, -80);
+  pop();
+  
+  push();
+  rotate(-20)
+  text('like', 30, -115);
+  text('blue', -40, -105);
+  pop();
+  
+  push();
+  rotate(-25);
+  text('a', -100, -60);
+  text('balloon', -90, -65);
+  text('go,', -100, -50);
+  text('and', -80, -55);
+  text('fought', 100, 50);
+  pop();
+  
+  push();
+  rotate(-45);
+  text('with', 113, 90);
+  pop();
+  
+  push();
+  rotate(-65);
+  text('other', 100, 130);
+  pop();
+  
+  push();
+  rotate(-85);
+  text('dogs,', 80, 165);
+  pop();
+  
+  push();
+  rotate(-5);
+  fill(255, 0, 0);
+  text('they', 280, -150);
+  text('say that,', 272, -140);
+  text('from space,', 265, -130);
+  text('the earth looks', 255, -120);
+  text("I'll", 225, -40);
+  text('throw', 210, -30);
+  text('i t  f o r', 200, -20);
+  text('you, Laika,', 185, -10);
+  text('if', 350, -40);
+  text("you'll", 350, -30);
+  text('c h a s e', 350, -20);
+  text('i            t', 350, -10);
+  text('dart through the', 255, 30);
+  text('stratosphere', 260, 40);
+  pop();
+  
+  push();
+  rotate(-5);
+  text('like', 283, -90);
+  pop();
+  
+  push();
+  rotate(-5);
+  fill(255, 100, 0);
+  text('like', 285, 70);
+  text('a comet,', 273, 80);
+  text('undeserving', 265, 95);
+  text('o   f    i   t   s', 265, 110);
+  text('f      a      t      e', 260, 125);
+  pop();
+
+  push();
+  rotate(45);
+  text('a', 130, -290);
+  pop();
+  
+  push();
+  rotate(135);
+  text('small,', -290, -150);
+  pop();
+  push();
+  
+  push();
+  rotate(225);
+  text('blue', -148, 255);
+  pop();
+  
+  push();
+  rotate(-50);
+  text('ball.', 237, 141);
+  pop();
+  
+  push();
+  fill(0,0,255);
+  text('and that, before you died, overheating',-190,170);
+  text('in that heavy, weightless cold, one of them',-190,180);
+  text('took you home, and you played with his kids.', -190, 190)
+  pop();
+}
+
+/*First Dog in Space, Brennig Davies
+
+They say that, from space,
+the Earth looks like a
+small, blue ball, but how
+did it look to you, Laika?
+From that shuttle like a balloon
+whose string they let go, and which
+they never trained for recall?
+
+They say you were a stray
+who never fought with other dogs,
+and that the clever people called you pet names
+through the wires of your shrinking cages,
+and that, before you died, overheating
+in that heavy, weightless cold, one of them
+took you home, and you played with his kids.
+
+They say that, from space,
+the Earth looks like
+a small, blue ball. I'll throw it
+for you Laika, if you'll chase it,
+
+dart through the stratosphere
+like a comet, undeserving
+of its fate
+*/
