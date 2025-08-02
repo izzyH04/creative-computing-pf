a17278036d09ed0482b66e9f29d0ef11a3790a19
izzyH04 <izzy.henshaw24@bathspa.ac.uk>
2025-08-02 17:18:50 +0100

    Experimented with diff angles

--- mouseGrid/obj.js
+++ mouseGrid/obj.js
@@ -7,11 +7,11 @@ class myRect {
   spin(){
     push()
     translate(this.x, this.y);
-    let angle = -atan2(-mouseX + this.x, -mouseY + this.y);
+    let angle = atan2(this.x-mouseX, this.y-mouseY);
     rotate(angle);
     
     noStroke();
-    fill(this.x-mouseX,this.y-mouseY,200);
+    fill(mouseX-this.x,mouseY-this.y,200);
     rect(10, 10, mouseX/20, mouseY/20);
     pop();
   }
